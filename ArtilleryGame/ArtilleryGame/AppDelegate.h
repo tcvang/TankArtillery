0daed7c5353044ac864df45aad519ddb40830e55
John Bender <john@generalui.com>
2012-05-03 11:48:04 -0700

    grading

--- ArtilleryGame/ArtilleryGame/AppDelegate.h
+++ ArtilleryGame/ArtilleryGame/AppDelegate.h
@@ -18,3 +18,19 @@
 @property (nonatomic, retain) UIWindow *window;
 
 @end
+
+
+/*
+ The app runs, plays well, and looks fairly good. Good first effort, and props for
+ teaching yourself Cocos2d.
+ 
+ I'm disappointed, though, that you merely implemented a tutorial app. Also, you
+ made some questionable decisions on coding style. For example, most of the code
+ in Player1 and Player2 is identical. Why not use object-oriented design principles
+ and have them both inherit from a Player superclass? You also avoid the standard
+ Model-View-Controller paradigm and have all your logic munged together, which is
+ admittedly harder to do when using OpenGL.
+ 
+ Grade: 80%
+ Course grade: 82% (B)
+*/
